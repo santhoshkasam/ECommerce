cb6a6e685ffbae3b6dd9db6788ccfc4a0e77dfa4
Sharada Kasam <sharadakasam@outlook.com>
2023-04-05 22:55:47 +0530

    Orders Controller

--- ECommerce.Api.Orders/Program.cs
+++ ECommerce.Api.Orders/Program.cs
@@ -1,8 +1,19 @@
+using ECommerce.Api.Orders.Db;
+using ECommerce.Api.Orders.Interfaces;
+using ECommerce.Api.Orders.Providers;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<OrdersDbContext>(option =>
+{
+    option.UseInMemoryDatabase("Orders");
+});
+builder.Services.AddAutoMapper(typeof(Program));
+builder.Services.AddScoped<IOrdersProvider, OrdersProvider>();
 
 var app = builder.Build();
 
